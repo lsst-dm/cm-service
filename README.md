959e0baadfebe86d26457ec08ef84dc20d863fc0
Fritz Mueller <fritzm@slac.stanford.edu>
2023-01-19 00:42:47 +0000

    squarebot safir project template

---
+++ README.md
@@ -0,0 +1,6 @@
+# cm-service
+
+Campaign management FastAPI service
+Learn more at https://cm-service.lsst.io
+
+cm-service is developed with [FastAPI](https://fastapi.tiangolo.com) and [Safir](https://safir.lsst.io).
---
+++ src/cmservice/main.py
@@ -0,0 +1,53 @@
+"""The main application factory for the cm-service service.
+
+Notes
+-----
+Be aware that, following the normal pattern for FastAPI services, the app is
+constructed when this module is loaded and is not deferred until a function is
+called.
+"""
+
+from importlib.metadata import metadata, version
+
+from fastapi import FastAPI
+from safir.dependencies.http_client import http_client_dependency
+from safir.logging import configure_logging, configure_uvicorn_logging
+from safir.middleware.x_forwarded import XForwardedMiddleware
+
+from .config import config
+from .handlers.external import external_router
+from .handlers.internal import internal_router
+
+__all__ = ["app", "config"]
+
+
+configure_logging(
+    profile=config.profile,
+    log_level=config.log_level,
+    name="cmservice",
+)
+configure_uvicorn_logging(config.log_level)
+
+app = FastAPI(
+    title="cm-service",
+    description=metadata("cm-service")["Summary"],
+    version=version("cm-service"),
+    openapi_url=f"/{config.name}/openapi.json",
+    docs_url=f"/{config.name}/docs",
+    redoc_url=f"/{config.name}/redoc",
+)
+"""The main FastAPI application for cm-service."""
+
+# Attach the routers.
+app.include_router(internal_router)
+app.include_router(external_router, prefix=f"/{config.name}")
+
+
+@app.on_event("startup")
+async def startup_event() -> None:
+    app.add_middleware(XForwardedMiddleware)
+
+
+@app.on_event("shutdown")
+async def shutdown_event() -> None:
+    await http_client_dependency.aclose()
