b4df848b05796ca4b288bfc3211bcb3bbc56a76b
Eman Ali <eali@swin.edu.au>
2024-03-14 18:23:35 +1100

    implement search campaigns

--- src/lsst/cmservice/web_app/pages/campaigns.py
+++ src/lsst/cmservice/web_app/pages/campaigns.py
@@ -4,9 +4,7 @@ from lsst.cmservice.db import Campaign
 
 
 async def search_campaigns(session, search_term):
-    print(search_term)
-    q = select(Campaign)
-    q.where(Campaign.fullname.contains(search_term))
+    q = select(Campaign).where(Campaign.name.contains(search_term))
     async with session.begin_nested():
         results = await session.scalars(q)
         return results.all()
