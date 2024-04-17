f13bcee13dc019dc72587e7d71a86f0ab9d8d4be
Eman Ali <eali@swin.edu.au>
2024-04-18 09:56:34 +1000

    retrieve real job and script stats

--- src/lsst/cmservice/web_app/utils/utils.py
+++ src/lsst/cmservice/web_app/utils/utils.py
@@ -4,14 +4,20 @@ from lsst.cmservice.common.enums import StatusEnum
 async def get_campaign_details(session, campaign):
     collections = await campaign.resolve_collections(session)
     jobs = await campaign.get_jobs(session)
-    for j in jobs:
-        print(f"{j.id} - {j.name} - {j.status}")
+    jobs_completed = [job for job in jobs if job.status == StatusEnum.accepted]
+    scripts = await campaign.get_scripts(session)
+    scripts_completed = [script for script in scripts if script.status == StatusEnum.accepted]
+    #     steps = await campaign.children(session)
+    #     for s in steps:
+    #         print(f"{s.id} - {s.name} - {s.status}")
     campaign_details = {
         "name": campaign.name,
         "lsst_version": campaign.data["lsst_version"],
         "root": collections["root"],
         "source": collections["campaign_source"],
         "status": map_status(campaign.status),
+        "jobs_completed": f"{len(jobs_completed)} of {len(jobs)} jobs completed",
+        "scripts_completed": f"{len(scripts_completed)} of {len(scripts)} scripts completed",
     }
     return campaign_details
 
