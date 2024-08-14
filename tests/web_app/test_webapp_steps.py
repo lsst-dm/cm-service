cf58f60ec18f8aacc2d6adb74d466221e24366bc
Eman Ali <eali@swin.edu.au>
2024-08-14 23:22:45 +1000

    api test for steps page

--- tests/web_app/test_webapp_steps.py
+++ tests/web_app/test_webapp_steps.py
@@ -1,4 +1,59 @@
+from unittest.mock import Mock
+
+import pytest
 from playwright.sync_api import sync_playwright, expect
+
+from lsst.cmservice import db
+from lsst.cmservice.common.enums import StatusEnum
+from lsst.cmservice.db import Step
+from lsst.cmservice.web_app.pages.steps import get_step_details
+
+
+@pytest.fixture()
+def mock_session():
+    yield Mock()
+
+
+@pytest.fixture()
+def mock_step():
+    step = Step(
+        id=1,
+        name="first_step",
+        parent_id=1,
+        fullname="first_production/first_campaign/first_step",
+        status=StatusEnum.accepted,
+    )
+    return step
+
+
+@pytest.fixture()
+def mock_groups():
+    yield [
+        db.Group(id=1, name="first_group", status=StatusEnum.accepted),
+        db.Group(id=1, name="second_group", status=StatusEnum.paused),
+        db.Group(id=1, name="second_group", status=StatusEnum.failed),
+        db.Group(id=1, name="second_group", status=StatusEnum.rejected),
+    ]
+
+
+@pytest.mark.asyncio
+async def test_get_step_details(mock_step, monkeypatch, mock_session, mock_groups):
+    async def mock_children(mock_session):
+        return mock_groups
+
+    monkeypatch.setattr(mock_step, "children", mock_children)
+    step_details = await get_step_details(mock_session, mock_step)
+    assert isinstance(step_details, dict)
+    assert step_details == {
+        "id": 1,
+        "name": "first_step",
+        "fullname": "first_production/first_campaign/first_step",
+        "status": "COMPLETE",
+        "no_groups": 4,
+        "no_groups_completed": 1,
+        "no_groups_need_attention": 1,
+        "no_groups_failed": 2,
+    }
 
 
 def test_steps_page() -> None:
