6e62d2728c5aec25527720769486b58a3fe62e9d
Eman Ali <eali@swin.edu.au>
2024-08-22 22:44:23 +1000

    add a commandline 'run-playwright' option to skip tests marked with the playwright marker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from collections.abc import AsyncIterator
 
+import pytest
 import pytest_asyncio
 import structlog
 from asgi_lifespan import LifespanManager
@@ -52,3 +53,26 @@ async def uvicorn_fixture(tmp_path_factory: TempPathFactory) -> AsyncIterator[Uv
     )
     yield my_uvicorn
     my_uvicorn.process.terminate()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--run-playwright",
+        action="store_true",
+        default=False,
+        help="run playwright tests",
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "playwright: mark test as a playwright test")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--run-playwright"):
+        # --run-playwright given in cli: do not skip playwright
+        return
+    skip_playwright = pytest.mark.skip(reason="need --run-playwright option to run")
+    for item in items:
+        if "playwright" in item.keywords:
+            item.add_marker(skip_playwright)
