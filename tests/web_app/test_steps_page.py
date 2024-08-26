ec61db549beb741b0de867eb43c4a8d6915f2195
Eman Ali <eali@swin.edu.au>
2024-08-26 13:12:22 +1000

    fix some tests

--- tests/web_app/test_steps_page.py
+++ tests/web_app/test_steps_page.py
@@ -71,27 +71,27 @@ def test_steps_page() -> None:
         # context.tracing.start(screenshots=True, snapshots=True, sources=True)
         page = context.new_page()
         # navigate to step list of the first campaign
-        page.goto("http://0.0.0.0:8080/web_app/campaign/1/steps/")
+        page.goto("http://0.0.0.0:8080/web_app/campaign/4/steps/")
         # check 1st step fullname is correct
-        expect(page.get_by_text("HSC_DRP-Prod/test_panda/step1")).not_to_be_empty()
+        expect(page.get_by_text("HSC_DRP-RC2/w_2024_26_DM-45061/step1")).not_to_be_empty()
         # check number of groups is right
-        expect(page.get_by_text("5 Group(s)")).not_to_be_empty()
+        expect(page.get_by_text("6 Group(s)")).not_to_be_empty()
         # check the campaign has 12 steps
-        expect(page.locator(".step-card")).to_have_count(12)
+        expect(page.locator(".step-card")).to_have_count(14)
         # check the first step card has a full width progress bar with green bg
         expect(page.locator(".step-card").first.filter(has=page.locator(".w-full"))).not_to_be_empty()
         expect(page.locator(".step-card").first.filter(has=page.locator(".bg-green-500"))).not_to_be_empty()
         # check campaign name is correct
-        expect(page.get_by_text("test_panda", exact=True)).not_to_be_empty()
+        expect(page.get_by_text("w_2024_26_DM-45061", exact=True)).not_to_be_empty()
         # check clicking the first step name opens step details page
         page.get_by_role("link", name="step1").click()
-        expect(page).to_have_url("http://0.0.0.0:8080/web_app/campaign/1/1/")
+        expect(page).to_have_url("http://0.0.0.0:8080/web_app/campaign/4/29/")
         # check clicking campaign name breadcrumb
         # in step details opens step list page
-        page.get_by_role("link", name="test_panda").click()
-        expect(page).to_have_url("http://0.0.0.0:8080/web_app/campaign/1/steps/")
+        page.get_by_role("link", name="w_2024_26_DM-45061").click()
+        expect(page).to_have_url("http://0.0.0.0:8080/web_app/campaign/4/steps/")
         # check clicking production name opens campaigns page
-        page.get_by_role("link", name="HSC_DRP-Prod").click()
+        page.get_by_role("link", name="HSC_DRP-RC2").click()
         expect(page).to_have_url("http://0.0.0.0:8080/web_app/campaigns/")
         # context.tracing.stop(path="trace2.zip")
         context.close()
