978b5c1e1b7dd858f65b83ff35dd2ecdde7435bb
Eric Charles <badass@stanford.edu>
2024-10-21 16:52:39 -0700

    Fixes and pragmas prompted by router unit tests

--- src/lsst/cmservice/routers/jobs.py
+++ src/lsst/cmservice/routers/jobs.py
@@ -33,7 +33,8 @@ router = APIRouter(
 # Attach functions to the router
 get_rows = wrappers.get_rows_no_parent_function(router, ResponseModelClass, DbClass)
 get_row = wrappers.get_row_function(router, ResponseModelClass, DbClass)
-get_row_by_fullname = wrappers.get_row_function(router, ResponseModelClass, DbClass)
+get_row_by_fullname = wrappers.get_row_by_fullname_function(router, ResponseModelClass, DbClass)
+get_row_by_name = wrappers.get_row_by_name_function(router, ResponseModelClass, DbClass)
 post_row = wrappers.post_row_function(
     router,
     ResponseModelClass,
