b80d506d9656d1b3b41dded63d053fd0e8ed2f96
Eric Charles <badass@stanford.edu>
2024-10-21 16:52:11 -0700

    Updates to routers unit tests

--- tests/routers/test_steps.py
+++ tests/routers/test_steps.py
@@ -1,7 +1,21 @@
+import os
+import uuid
+
 import pytest
 from httpx import AsyncClient
 
+from lsst.cmservice import models
+from lsst.cmservice.common.enums import LevelEnum
 from lsst.cmservice.config import config
+
+from .util_functions import (
+    check_and_parse_repsonse,
+    check_get_methods,
+    check_scripts,
+    check_update_methods,
+    create_tree,
+    delete_all_productions,
+)
 
 
 @pytest.mark.asyncio()
@@ -9,19 +23,38 @@ from lsst.cmservice.config import config
 async def test_steps_api(client: AsyncClient) -> None:
     """Test `/steps` API endpoint."""
 
-    # Get list, it should be empty
-    response = await client.get(f"{config.prefix}/step/list")
-    assert response.status_code == 200
-    data = response.json()
-    assert isinstance(data, list)
-    # sids_expected = set(sids)
-    sids_retrieved = {step["id"] for step in data}
-    assert not sids_retrieved
+    # generate a uuid to avoid collisions
+    uuid_int = uuid.uuid1().int
 
-    # Verify an individual get
-    # response = await client.get(f"{config.prefix}/step/get/{data[0]['id']}")
-    # assert response.status_code == 200
-    # data = response.json()
-    # assert data["id"] == sids[0]
-    # assert data["parent_id"] == 13
-    # assert data["name"] == "isr"
+    os.environ["CM_CONFIGS"] = "examples"
+
+    # intialize a tree down to one level lower
+    await create_tree(client, LevelEnum.group, uuid_int)
+
+    response = await client.get(f"{config.prefix}/step/list")
+    steps = check_and_parse_repsonse(
+        response,
+        list[models.Step],
+    )
+    entry = steps[0]
+
+    # check get methods
+    await check_get_methods(client, entry, "step", models.Step, models.Campaign)
+
+    # check update methods
+    await check_update_methods(client, entry, "step", models.Step)
+
+    # check scripts
+    await check_scripts(client, entry, "step")
+
+    # delete everything we just made in the session
+    await delete_all_productions(client)
+
+    # confirm cleanup
+    response = await client.get(f"{config.prefix}/production/list")
+    productions = check_and_parse_repsonse(
+        response,
+        list[models.Production],
+    )
+
+    assert len(productions) == 0
