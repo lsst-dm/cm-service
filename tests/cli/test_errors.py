c3ab9f9889fbab04369103fe23cd432616bf1c2c
Eric Charles <badass@stanford.edu>
2024-11-05 16:34:22 -0800

    Added cli tests for errors and loaders

---
+++ tests/cli/test_errors.py
@@ -0,0 +1,55 @@
+import pytest
+from click.testing import CliRunner
+from safir.testing.uvicorn import UvicornProcess
+
+from lsst.cmservice import models
+from lsst.cmservice.cli.commands import client_top
+from lsst.cmservice.client.clientconfig import client_config
+from lsst.cmservice.config import config
+
+from .util_functions import (
+    check_and_parse_result,
+    delete_all_rows,
+)
+
+
+@pytest.mark.asyncio()
+async def test_error_create(uvicorn: UvicornProcess) -> None:
+    """Test error matching in pipetask_error_type.match.
+
+    Correctly match a real error to the error_type database and fail to match a
+    fake error which is not in the database.
+    """
+
+    client_config.service_url = f"{uvicorn.url}{config.prefix}"
+    runner = CliRunner()
+
+    result = runner.invoke(
+        client_top,
+        "pipetask_error_type create --error_source manifest --error_flavor configuration "
+        "--error_action review --task_name skyObjectMean "
+        '--diagnostic_message "The error message from a regular pipetask error" --output yaml',
+    )
+
+    e1 = check_and_parse_result(result, models.PipetaskErrorType)
+    assert e1.task_name == "skyObjectMean"
+
+    delete_all_rows(runner, client_top, "pipetask_error_type", models.PipetaskErrorType)
+
+
+@pytest.mark.asyncio()
+async def test_load_error_types_cli(uvicorn: UvicornProcess) -> None:
+    """Test `error_type` db table."""
+
+    client_config.service_url = f"{uvicorn.url}{config.prefix}"
+    runner = CliRunner()
+
+    result = runner.invoke(client_top, "load error-types --yaml_file examples/error_types.yaml")
+    assert result.exit_code == 0
+
+    # FIXME, fullname matching isn't quite working
+    # result = runner.invoke(client_top, "load error-types --allow_update
+    # --yaml_file examples/error_types.yaml")
+    # assert result.exit_code == 0
+
+    delete_all_rows(runner, client_top, "pipetask_error_type", models.PipetaskErrorType)
---
+++ tests/cli/test_trivial.py
@@ -0,0 +1,42 @@
+import pytest
+from click.testing import CliRunner
+from safir.testing.uvicorn import UvicornProcess
+
+from lsst.cmservice import models
+from lsst.cmservice.cli.commands import client_top
+from lsst.cmservice.client.clientconfig import client_config
+from lsst.cmservice.config import config
+
+from .util_functions import (
+    check_and_parse_result,
+    cleanup,
+)
+
+
+@pytest.mark.asyncio()
+async def test_cli_trivial_campaign(uvicorn: UvicornProcess) -> None:
+    """Test fake end to end run using example/example_trivial.yaml"""
+
+    client_config.service_url = f"{uvicorn.url}{config.prefix}"
+    runner = CliRunner()
+
+    yaml_file = "examples/example_trivial.yaml"
+
+    # Just use table output, we don't need the return value
+    result = runner.invoke(client_top, f"load specification --yaml_file {yaml_file}")
+    assert result.exit_code == 0
+
+    result = runner.invoke(client_top, f"load specification --yaml_file {yaml_file} --allow_update")
+    assert result.exit_code == 0
+
+    result = runner.invoke(
+        client_top,
+        f"load campaign --yaml_file {yaml_file} --name test --parent_name trivial "
+        "--spec_block_name campaign --spec_name trivial_htcondor "
+        "--campaign_yaml examples/start_trivial.yaml --output yaml",
+    )
+    campaign = check_and_parse_result(result, models.Campaign)
+    assert campaign.name == "test"
+
+    # delete everything we just made in the session
+    cleanup(runner, client_top)
