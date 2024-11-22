a06a116c503b25fbde2c653b0301a4d232dfe927
Eman Ali <eali@swin.edu.au>
2024-11-22 11:48:24 +1100

    clean request code, Distinguish between reset and review script in the modal dialog. Also disable the button if the script state doesn't allow resetting or reviewing, sort scripts, jobs, groups by id, move data objects to the tag that is reloaded by htmx in step details and group details pages, change accept/reject script behavior
    add script log url to reset script dialog

--- src/lsst/cmservice/web_app/static/js/script-grid-utils.js
+++ src/lsst/cmservice/web_app/static/js/script-grid-utils.js
@@ -11,36 +11,60 @@ const resetFromReviewable = {
 };
 
 const updateStatus = () => {
-    let resetObj = {
-        fullname: String(document.querySelector("#scriptFullname").innerText),
-        status: parseInt(document.querySelector("#targetStatus").value),
-    };
+    const rowIndex = parseInt(document.querySelector("#rowIndex").innerText);
+    const newStatus = parseInt(document.querySelector("#targetStatus").value);
+    const fullname = String(document.querySelector("#scriptFullname").innerText)
+    const scriptId = scriptGridApi.getRowNode(rowIndex).data.id;
 
-    let resetJson = JSON.stringify(resetObj);
+    let url;
+    let resetJson = null;
 
+    // accept script
+    if(newStatus === 5){
+        url = `/cm-service/v1/script/action/${scriptId}/accept`;
+    }
+    // reject script
+    else if(newStatus === -3){
+        url = `/cm-service/v1/script/action/${scriptId}/reject`;
+    }
+    // reset script
+    else {
+        let resetObj = {
+            fullname: fullname,
+            status: newStatus,
+        };
+
+        resetJson = JSON.stringify(resetObj);
+        url = reset_script_endpoint;
+    }
+
+    // send request
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', reset_script_endpoint, true);
+    xhr.open('POST', url, true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.onload = function () {
         console.log(xhr.responseText);
 
         if (xhr.status === 201) {
-            const rowIndex = Number(document.querySelector("#rowIndex").innerText);
-            const newStatus = resetObj.status;
-            scriptGridApi.getRowNode(rowIndex).setDataValue("status", newStatus);
+            scriptGridApi.getRowNode(rowIndex).setDataValue("status", document.querySelector("#targetStatus").selectedOptions[0].text);
+            scriptGridApi.refreshCells({
+                force: true,
+                suppressFlash: false,
+              });
             resetScriptModal.close();
         } else {
             resetScriptModal.close();
             errorModal.showModal();
-            errorMessage.innerText = 'Error resetting script!';
+            errorMessage.innerText = 'Error accepting script!';
         }
     };
     xhr.onerror = function () {
         console.error("Request failed");
-        alert('Error resetting script!');
+        alert('Error accepting script!');
         resetScriptModal.close();
     };
     xhr.send(resetJson);
+
 };
 
 class ResetButtonCellRenderer {
@@ -48,38 +72,61 @@ class ResetButtonCellRenderer {
         this.params = params;
         this.eGui = document.createElement('button');
         this.eGui.innerText = 'Reset';
+        let currentStatus = params.data.status;
+
+        if(currentStatus === "FAILED" || currentStatus === "REJECTED") {
+            this.eGui.innerText = 'Reset';
+        } else if(currentStatus === "REVIEWABLE") {
+            this.eGui.innerText = 'Review';
+        } else {
+            this.eGui.disabled = true;
+            this.eGui.setAttribute('class', 'mt-2 rounded bg-white px-2 py-1 text-xs font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50');
+            return;
+        }
+
+        this.eGui.disabled = false;
         this.eGui.setAttribute('class', 'mt-2 rounded bg-white px-2 py-1 text-xs font-semibold text-teal-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50');
 
         this.eGui.addEventListener('click', () => {
+            document.querySelector("#rowIndex").innerText = params.node.rowIndex;
+             document.querySelector("#scriptFullname").innerText = params.data.fullname;
+             const scriptLogElement = document.querySelector("#scriptLogUrl");
+             scriptLogElement.innerHTML = "";
+             if(params.data.log_url) {
+                 let logUrlHref = document.createElement('a');
+                 logUrlHref.setAttribute('class', 'text-teal-700 underline underline-offset-4 decoration-1 hover:text-gray-500');
+                 logUrlHref.href = params.data.log_url;
+                 logUrlHref.innerText = params.data.log_url;
+                 scriptLogElement.appendChild(logUrlHref);
+             } else {
+                 scriptLogElement.innerText = "-";
+             }
 
-         document.querySelector("#rowIndex").innerText = params.node.rowIndex;
-         document.querySelector("#scriptFullname").innerText = params.data.fullname;
-
-         let statusDropdown = document.querySelector("#targetStatus");
-         statusDropdown.options.length = 0;
-
-         let currentStatus = params.data.status;
-
-         if(currentStatus === "FAILED" || currentStatus === "REJECTED"){
-             Object.keys(resetFromRejected).forEach(state => {
-                 let option = document.createElement('option');
-                 option.setAttribute('value', resetFromRejected[state]);
-                 option.innerText = state;
-                 statusDropdown.add(option);
-             });
-         } else if (currentStatus === "REVIEWABLE") {
-             Object.keys(resetFromReviewable).forEach(state => {
-                 let option = document.createElement('option');
-                 option.setAttribute('value', resetFromReviewable[state]);
-                 option.innerText = state;
-                 statusDropdown.add(option);
-             });
-         } else {
-             errorModal.showModal();
-             errorMessage.innerText = `Cannot reset a script from ${currentStatus} status`;
-             return;
-         }
-         resetScriptModal.showModal();
+             let statusDropdown = document.querySelector("#targetStatus");
+             statusDropdown.options.length = 0;
+             let currentStatus = params.data.status;
+             if(currentStatus === "FAILED" || currentStatus === "REJECTED"){
+                 document.querySelector('#modal-title').innerText = "Reset Script";
+                 Object.keys(resetFromRejected).forEach(state => {
+                     let option = document.createElement('option');
+                     option.setAttribute('value', resetFromRejected[state]);
+                     option.innerText = state;
+                     statusDropdown.add(option);
+                 });
+             } else if (currentStatus === "REVIEWABLE") {
+                 document.querySelector('#modal-title').innerText = "Review Script";
+                 Object.keys(resetFromReviewable).forEach(state => {
+                     let option = document.createElement('option');
+                     option.setAttribute('value', resetFromReviewable[state]);
+                     option.innerText = state;
+                     statusDropdown.add(option);
+                 });
+             } else {
+                 errorModal.showModal();
+                 errorMessage.innerText = `Cannot reset a script from ${currentStatus} status`;
+                 return;
+             }
+             resetScriptModal.showModal();
         });
     }
 
