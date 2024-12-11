c652bf86cfa14bf3d2192578ec6d1fcda047d1eb
Eman Ali <eali@swin.edu.au>
2024-12-11 15:20:44 +1100

    user fetch api, add another show log button in the grid

--- src/lsst/cmservice/web_app/static/js/script-grid-utils.js
+++ src/lsst/cmservice/web_app/static/js/script-grid-utils.js
@@ -10,7 +10,7 @@ const resetFromReviewable = {
     REJECTED: -3,
 };
 
-const updateStatus = () => {
+const updateStatus = async () => {
     const rowIndex = parseInt(document.querySelector("#rowIndex").innerText);
     const newStatus = parseInt(document.querySelector("#targetStatus").value);
     const fullname = String(document.querySelector("#scriptFullname").innerText)
@@ -39,132 +39,124 @@ const updateStatus = () => {
     }
 
     // send request
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.onload = function () {
-        if (xhr.status === 201) {
-            // update script status in grid
-            scriptGridApi
-                .getRowNode(rowIndex)
-                .setDataValue("status", document.querySelector("#targetStatus").selectedOptions[0].text);
-            // refresh grid cells on client side to disable/enable reset button according to new status
-            scriptGridApi.refreshCells({
-                force: true,
-                suppressFlash: false,
-              });
-            resetScriptModal.close();
-        } else {
-            resetScriptModal.close();
-            errorModal.showModal();
-            errorMessage.innerText = JSON.parse(xhr.responseText).detail;
+    const response = await fetch(url, {
+        method: "POST",
+        body: resetJson,
+        headers: {
+            "Content-Type": "application/json"
         }
-    };
-    xhr.onerror = function () {
+    });
+
+    if(response.ok) {
+        // update script status in grid
+        scriptGridApi
+            .getRowNode(rowIndex)
+            .setDataValue("status", document.querySelector("#targetStatus").selectedOptions[0].text);
+        // refresh grid cells on client side to disable/enable reset button according to new status
+        scriptGridApi.refreshCells({
+            force: true,
+            suppressFlash: false,
+          });
+        resetScriptModal.close();
+    } else {
         resetScriptModal.close();
         errorModal.showModal();
-        errorMessage.innerText = xhr.responseText;
-    };
-    xhr.send(resetJson);
-
+        errorMessage.innerText = JSON.parse(await response.text()).detail;
+    }
 };
 
-const readScriptLog = (log_url) => {
-
-    const xhr = new XMLHttpRequest();
-
+const readScriptLog = async (logURL) => {
     const url = "/web_app/read-script-log";
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                const response = JSON.parse(xhr.responseText);
-                // show file content from response in the textarea
-                document.querySelector('#scriptLogContent').value = response.content;
-                scriptLogModal.showModal();
-            } else {
-                errorModal.showModal();
-                errorMessage.innerText = JSON.parse(xhr.responseText).detail;
-            }
+    const res  = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({ log_path: logURL}),
+        headers: {
+            "Content-Type": "application/json"
         }
-    };
+    });
+    if(!res.ok) {
+        errorModal.showModal();
+        errorMessage.innerText = JSON.parse(await res.text()).detail;
+    } else {
+        let response = await res.json()
+        document.querySelector('#scriptLogContent').value = response.content;
+        scriptLogModal.showModal();
+    }
+};
 
-    xhr.open("POST", url, true);
-
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    const requestData = JSON.stringify({ log_path: log_url});
-    xhr.send(requestData);
-}
+const fillTargetStatusDropdown = (statusEnum) => {
+    let statusDropdown = document.querySelector("#targetStatus");
+    statusDropdown.options.length = 0;
+    Object.keys(statusEnum).forEach(state => {
+        let option = document.createElement('option');
+        option.setAttribute('value', statusEnum[state]);
+        option.innerText = state;
+        statusDropdown.add(option);
+    });
+};
 
 class ResetButtonCellRenderer {
     init(params) {
         this.params = params;
-        this.eGui = document.createElement('button');
-        // change button text and also enable/disable according to status
-        let currentStatus = params.data.status;
-        if(currentStatus === "FAILED" || currentStatus === "REJECTED") {
-            this.eGui.innerText = 'Reset';
-        } else if(currentStatus === "REVIEWABLE") {
-            this.eGui.innerText = 'Review';
+        this.eGui = document.createElement('div');
+
+        let gridShowLogBtn = document.createElement('button');
+        gridShowLogBtn.innerText = "Show Log";
+        if(params.data.log_url){
+            gridShowLogBtn.setAttribute("class", 'mt-2 rounded bg-white px-2 py-1 text-xs font-semibold text-teal-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50');
+            gridShowLogBtn.addEventListener('click', async () => await readScriptLog(params.data.log_url));
+            gridShowLogBtn.disabled = false;
         } else {
-            // button is disabled if the current status isn't FAILED/REJECTED/REVIEWABLE
-            this.eGui.innerText = 'Reset';
-            this.eGui.disabled = true;
-            this.eGui.setAttribute('class', 'mt-2 rounded bg-white px-2 py-1 text-xs font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50');
+            gridShowLogBtn.setAttribute("class", 'mt-2 rounded bg-white px-2 py-1 text-xs font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50');
+            gridShowLogBtn.disabled = true;
+        }
+        this.eGui.appendChild(gridShowLogBtn);
+
+        let resetBtn = document.createElement('button');
+        // change button text and also show/hide according to status
+        let currentStatus = params.data.status;
+        this.eGui.appendChild(resetBtn);
+
+        if(currentStatus === "FAILED" || currentStatus === "REJECTED") {
+            resetBtn.innerText = 'Reset';
+        } else if(currentStatus === "REVIEWABLE") {
+            resetBtn.innerText = 'Review';
+        } else {
+            // button is hidden if the current status isn't FAILED/REJECTED/REVIEWABLE
+            resetBtn.hidden = true;
             return;
         }
-        // enable reset/review button
-        this.eGui.disabled = false;
-        this.eGui.setAttribute('class', 'mt-2 rounded bg-white px-2 py-1 text-xs font-semibold text-teal-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50');
+        // show reset/review button
+        resetBtn.hidden = false;
+        resetBtn.setAttribute('class', 'ml-2 mt-2 rounded bg-white px-2 py-1 text-xs font-semibold text-teal-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50');
 
-        this.eGui.addEventListener('click', () => {
+        resetBtn.addEventListener('click', () => {
             document.querySelector("#rowIndex").innerText = params.node.rowIndex;
              document.querySelector("#scriptFullname").innerText = params.data.fullname;
 
-             // show script log path if it isn't null, otherwise show "-"
+             // show script log path and button if it isn't null, otherwise show "-" and hide button
              const scriptLogElement = document.querySelector("#scriptLogUrl");
-             scriptLogElement.innerHTML = "";
              if(params.data.log_url) {
-                 let logUrl = document.createElement('span');
-                 logUrl.innerText = params.data.log_url;
-                 scriptLogElement.appendChild(logUrl);
-
-                 // add a button to show log content dialog
-                 let showLogBtn = document.createElement('button');
-                 showLogBtn.innerText = 'Show Log';
-                 showLogBtn.setAttribute('class', 'mt-2 rounded bg-white px-2 py-1 text-xs font-semibold text-teal-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50');
-                 showLogBtn.addEventListener('click', (e) => {
-                     readScriptLog(params.data.log_url);
-                 });
-                 scriptLogElement.appendChild(showLogBtn);
-
+                 scriptLogElement.innerText = params.data.log_url;
+                 showLogBtn.hidden = false;
              } else {
                  scriptLogElement.innerText = "-";
+                 showLogBtn.hidden = true;
              }
+             showLogBtn.addEventListener("click", async() => await readScriptLog(params.data.log_url));
+
              // create target status dropdown according to current status
-             let statusDropdown = document.querySelector("#targetStatus");
-             statusDropdown.options.length = 0;
              let currentStatus = params.data.status;
              if(currentStatus === "FAILED" || currentStatus === "REJECTED"){
                  document.querySelector('#reset-modal-title').innerText = "Reset Script";
-                 Object.keys(resetFromRejected).forEach(state => {
-                     let option = document.createElement('option');
-                     option.setAttribute('value', resetFromRejected[state]);
-                     option.innerText = state;
-                     statusDropdown.add(option);
-                 });
+                 fillTargetStatusDropdown(resetFromRejected);
              } else if (currentStatus === "REVIEWABLE") {
                  document.querySelector('#reset-modal-title').innerText = "Review Script";
-                 Object.keys(resetFromReviewable).forEach(state => {
-                     let option = document.createElement('option');
-                     option.setAttribute('value', resetFromReviewable[state]);
-                     option.innerText = state;
-                     statusDropdown.add(option);
-                 });
+                 fillTargetStatusDropdown(resetFromReviewable);
              }
              resetScriptModal.showModal();
         });
+
     }
 
     getGui() {
@@ -174,11 +166,14 @@ class ResetButtonCellRenderer {
 
 
 const resetScriptModal = document.querySelector("#modalDialog");
+
 const closeResetModalBtn = document.querySelector("[close-reset-modal]");
 closeResetModalBtn.addEventListener("click", () => resetScriptModal.close());
 
 const confirmResetBtn = document.querySelector("[confirm-reset]");
 confirmResetBtn.addEventListener("click", updateStatus);
+
+const showLogBtn = document.querySelector("#showLogBtn");
 
 const errorModal = document.querySelector("#errorModal");
 const closeErrModalBtn = document.querySelector("[close-error-modal]");
