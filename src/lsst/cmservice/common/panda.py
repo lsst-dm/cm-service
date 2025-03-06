b69557e373602cc51c881a716f743aeec4fea0a0
Toby Jennings <tobyj@slac.stanford.edu>
2025-03-06 13:37:27 -0600

    Update panda token logic
    Default expiry to None.
    Decode expiry from token only when needed.
    Make panda token checking a noop if panda is unconfigured.
    Check panda token before building bps submit environment.

--- src/lsst/cmservice/common/panda.py
+++ src/lsst/cmservice/common/panda.py
@@ -6,6 +6,7 @@ import os
 from collections.abc import Generator
 from contextlib import contextmanager
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import httpx
 from pandaclient.openidc_utils import decode_id_token
@@ -29,7 +30,32 @@ def http_client() -> Generator[httpx.Client]:
 
 
 def refresh_panda_token(url: str, data: dict[str, str]) -> str | None:
-    """Refresh a panda auth token."""
+    """Refresh a panda auth token.
+
+    Notes
+    -----
+    This function updates the configuration parameters object as a side-effect
+    while also returning the idtoken; callers may or may not use the return
+    value
+
+    Returns
+    -------
+    The current value of the PanDA id token as known by the configuration
+    parameter, which may be `None` if no id token has yet been successfully
+    determined.
+
+    Raises
+    ------
+    HTTPStatusError
+        Raised if the API call to a PanDA endpoint fails for status.
+
+    json.JSONDecodeError
+        Raised if a successful response body cannot be correctly parsed as JSON
+
+    KeyError
+        Raised if the response from a PanDA API does not contain an expected
+        key.
+    """
     with http_client() as session:
         response = session.post(
             url=url, data=data, headers={"content-type": "application/x-www-form-urlencoded"}
@@ -46,6 +72,10 @@ def refresh_panda_token(url: str, data: dict[str, str]) -> str | None:
     # - update token expiry
     decoded_token = decode_id_token(config.panda.id_token)
     config.panda.token_expiry = float(decoded_token["exp"])  # type: ignore
+    if TYPE_CHECKING:
+        # the validation machinery of the pyantic field handles conversion
+        # from float to datetime.
+        assert isinstance(config.panda.token_expiry, datetime.datetime)
     return config.panda.id_token
 
 
@@ -74,6 +104,10 @@ def get_panda_token() -> str | None:
     determined from the panda url and the oidc VO.
     """
 
+    # If no panda configuration exists in the application, return early
+    if config.panda.url is None:
+        return None
+
     # If a token has been added to the configuration object, use it instead of
     # loading one from disk
     try:
@@ -90,11 +124,15 @@ def get_panda_token() -> str | None:
     # Determine whether the token should be renewed
     # The token expiry time is part of the encoded token
     try:
-        decoded_token = decode_id_token(config.panda.id_token)
-        # TODO if "exp" not in decoded_token: ...
-        config.panda.token_expiry = float(decoded_token["exp"])  # type: ignore
+        if config.panda.token_expiry is None:
+            decoded_token = decode_id_token(config.panda.id_token)
+            config.panda.token_expiry = float(decoded_token["exp"])  # type: ignore
+        if TYPE_CHECKING:
+            # the validation machinery of the pyantic field handles conversion
+            # from float to datetime.
+            assert isinstance(config.panda.token_expiry, datetime.datetime)
     except Exception:
-        # FIXME: this should generally be an AttributeError but the 3rdparty
+        # NOTE: this should generally be an AttributeError but the 3rdparty
         #        function may change its operation.
         # If current id_token is None or otherwise not decodable, we will get a
         # new one from the refresh operation
@@ -104,6 +142,7 @@ def get_panda_token() -> str | None:
     if (config.panda.token_expiry - now_utc) < datetime.timedelta(config.panda.renew_after):
         if config.panda.auth_config_url is None:
             logger.error("There is no PanDA auth config url known to the service, cannot refresh token.")
+            logger.warning("The current PanDA id token may be invalid.")
             return config.panda.id_token
 
         try:
@@ -125,7 +164,10 @@ def get_panda_token() -> str | None:
                 )
 
             _ = refresh_panda_token(token_endpoint, data)
-        except httpx.HTTPStatusError:
+        except (httpx.HTTPStatusError, json.JSONDecodeError, KeyError):
+            # Error classes could include http status, malformed responses, or
+            # responses with missing keys, either in this function or in the
+            # token refresh function.
             logger.exception()
 
     return config.panda.id_token
