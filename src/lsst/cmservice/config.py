9352918c7fd648811d4287b0bd4fca06430a67a8
Toby Jennings <tobyj@slac.stanford.edu>
2025-06-18 11:32:33 -0500

    feat(db): apply pool configurations to db session

--- src/lsst/cmservice/config.py
+++ src/lsst/cmservice/config.py
@@ -8,6 +8,7 @@ from pydantic import (
     AliasChoices,
     BaseModel,
     Field,
+    SecretStr,
     computed_field,
     field_serializer,
     field_validator,
@@ -545,7 +546,7 @@ class DatabaseConfiguration(BaseModel):
         description="The URL for the cm-service database",
     )
 
-    password: str | None = Field(
+    password: SecretStr | None = Field(
         default=None,
         description="The password for the cm-service database",
     )
@@ -558,6 +559,31 @@ class DatabaseConfiguration(BaseModel):
     echo: bool = Field(
         default=False,
         description="SQLAlchemy engine echo setting for the cm-service database",
+    )
+
+    max_overflow: int = Field(
+        default=10,
+        description="Maximum connection overflow allowed for QueuePool.",
+    )
+
+    pool_size: int = Field(
+        default=5,
+        description="Number of open connections kept in the QueuePool",
+    )
+
+    pool_recycle: int = Field(
+        default=-1,
+        description="Timeout in seconds before connections are recycled",
+    )
+
+    pool_timeout: int = Field(
+        default=30,
+        description="Wait timeout for acquiring a connection from the pool",
+    )
+
+    pool_fields: set[str] = Field(
+        default={"max_overflow", "pool_size", "pool_recycle", "pool_timeout"},
+        description="Set of fields used for connection pool configuration",
     )
 
 
