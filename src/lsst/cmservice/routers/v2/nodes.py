9eb5fe3f92408e934a226e60cd9393138fab13a6
Toby Jennings <tobyj@slac.stanford.edu>
2025-07-18 17:38:03 -0500

    feat(api) - Add HEAD routes to pair with resource GETs - Implement graph node replace, insert, append - Implement campaign detail resource - Support DeepDiff delta bytes for PATCHes

--- src/lsst/cmservice/routers/v2/nodes.py
+++ src/lsst/cmservice/routers/v2/nodes.py
@@ -8,7 +8,8 @@ from collections.abc import Sequence
 from typing import TYPE_CHECKING, Annotated
 from uuid import UUID, uuid5
 
-from fastapi import APIRouter, Depends, HTTPException, Query, Request, Response
+from deepdiff import Delta
+from fastapi import APIRouter, Body, Depends, HTTPException, Query, Request, Response
 from pydantic import UUID5
 from sqlmodel import col, select
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -69,19 +70,19 @@ async def read_nodes_collection(
         raise HTTPException(status_code=500, detail=f"{str(msg)}") from msg
 
 
-@router.get(
-    "/{node_name}",
-    summary="Get node detail",
-)
+@router.get("/{node_name}", response_model=Node, summary="Get single node detail")
+@router.head("/{node_name}", response_model=None, summary="Get single node headers")
 async def read_node_resource(
     request: Request,
     response: Response,
     node_name: str,
     session: Annotated[AsyncSession, Depends(db_session_dependency)],
-    campaign_id: Annotated[UUID5 | None, Query(validation_alias="campaign_id", alias="campaign-id")] = None,
-) -> Node:
-    """Fetch a single node from the database given either the node id
-    or its name.
+    namespace: Annotated[UUID5 | None, Query(validation_alias="campaign_id", alias="campaign-id")] = None,
+    version: Annotated[int | None, Query()] = None,
+) -> Node | None:
+    """Fetch a single node from the database given either the node id or its
+    name together with a namespace; if no version is provided, the "latest"
+    version of the node is returned.
     """
     s = select(Node)
     # The input could be a campaign UUID or it could be a literal name.
@@ -92,20 +93,29 @@ async def read_node_resource(
         # node name by itself is not sufficient to identity a single node in
         # the database, so we must also constrain the request with the campaign
         # namespace or raise an error.
-        if campaign_id is None:
+        if namespace is None:
             raise HTTPException(
                 status_code=400, detail="Cannot locate Node by name alone. Try including `?campaign-id=...`"
             )
-        s = s.where(Node.name == node_name).where(Node.namespace == campaign_id)
+        s = s.where(Node.name == node_name).where(Node.namespace == namespace)
 
-    node = (await session.exec(s)).one_or_none()
+        if version is None:
+            s = s.order_by(col(Node.version).desc())
+        else:
+            s = s.where(Node.version == version)
+
+    node = (await session.exec(s.limit(1))).one_or_none()
     if node is None:
         raise HTTPException(status_code=404)
     response.headers["Self"] = request.url_for("read_node_resource", node_name=node.id).__str__()
     response.headers["Campaign"] = request.url_for(
         "read_campaign_resource", campaign_name_or_id=node.namespace
     ).__str__()
-    return node
+
+    if request.method == "HEAD":
+        return None
+    else:
+        return node
 
 
 @router.post(
@@ -179,7 +189,7 @@ async def update_node_resource(
     response: Response,
     session: Annotated[AsyncSession, Depends(db_session_dependency)],
     node_name_or_id: str,
-    patch_data: Sequence[JSONPatch],
+    patch_data: Annotated[bytes, Body()] | Sequence[JSONPatch],
 ) -> Node:
     """Partial update method for nodes.
 
@@ -198,17 +208,17 @@ async def update_node_resource(
     - This API always targets the latest version of a manifest when applying
       a patch. This requires and maintains a "linear" sequence of versions;
       it is not permissible to "patch" a previous version and create a "tree"-
-      like history of manifests. For exmaple, every manifest may be diffed
+      like history of manifests. For example, every manifest may be diffed
       against any previous version without having to consider branches.
     """
     use_rfc6902 = False
+    use_deepdiff = False
     if request.headers["Content-Type"] == "application/json-patch+json":
         use_rfc6902 = True
+    elif request.headers["Content-Type"] == "application/octet-stream":
+        use_deepdiff = True
     else:
         raise HTTPException(status_code=406, detail="Unsupported Content-Type")
-
-    if TYPE_CHECKING:
-        assert use_rfc6902
 
     s = select(Node)
     # The input could be a UUID or it could be a literal name.
@@ -232,17 +242,24 @@ async def update_node_resource(
     new_manifest["version"] += 1
     new_manifest["id"] = uuid5(new_manifest["namespace"], f"{new_manifest['name']}.{new_manifest['version']}")
 
-    for patch in patch_data:
-        try:
-            apply_json_patch(patch, new_manifest)
-        except JSONPatchError as e:
-            raise HTTPException(
-                status_code=422,
-                detail=f"Unable to process one or more patch operations: {e}",
-            )
+    if use_rfc6902:
+        if TYPE_CHECKING:
+            assert type(patch_data) is Sequence[JSONPatch]
+        for patch in patch_data:
+            try:
+                apply_json_patch(patch, new_manifest)
+            except JSONPatchError as e:
+                raise HTTPException(
+                    status_code=422,
+                    detail=f"Unable to process one or more patch operations: {e}",
+                )
+    elif use_deepdiff:
+        if TYPE_CHECKING:
+            assert isinstance(patch_data, bytes)
+        new_manifest["configuration"] += Delta(patch_data)
 
     # create Manifest from new_manifest, add to session, and commit
-    new_manifest["metadata"] |= {"crtime": element_time()}
+    new_manifest["metadata"] = {"crtime": element_time()}
     new_manifest_db = Node.model_validate(new_manifest)
     session.add(new_manifest_db)
     await session.commit()
