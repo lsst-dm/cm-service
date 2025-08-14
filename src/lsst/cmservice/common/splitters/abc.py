4c3cf1a13fd6f461af30c16bd64495465cbae636
Toby Jennings <tobyj@slac.stanford.edu>
2025-08-14 10:36:47 -0500

    feat(group): Implement group splitters

---
+++ src/lsst/cmservice/common/splitters/abc.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from collections.abc import AsyncGenerator, Mapping, Sequence
+from typing import Any
+
+from ..enums import SplitterEnum
+
+
+class Splitter(ABC):
+    """An abstract Splitter class that implements rules for generating Butler
+    query predicates for a group.
+    """
+
+    __kind__: SplitterEnum
+
+    def __init__(self, *args: Sequence, **kwargs: Mapping[str, Any]): ...
+
+    @abstractmethod
+    async def split(self) -> AsyncGenerator[str, Any]:
+        """An abstract split method that yields predicate strings."""
+        if False:
+            yield "1"  # type: ignore[unreachable]
