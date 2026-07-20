f386efff8c6728386f3a8b95410b7f7f7ef679bd
Toby Jennings <tobyj@slac.stanford.edu>
2026-07-20 12:27:59 -0500

    feat: refactor legacy notification system

---
+++ src/lsst/cmservice/notifications/transport/slack.py
@@ -0,0 +1,159 @@
+import json
+from typing import TYPE_CHECKING
+
+import httpx
+from sqlalchemy.exc import NoResultFound
+
+from lsst.cmservice.models.db.campaigns import ActivityLog
+from lsst.cmservice.models.db.notifications import NotificationLabel
+from lsst.cmservice.models.enums import NotificationLabelEnum, StatusEnum
+from lsst.cmservice.models.lib.logging import LOGGER
+
+from ...config import config
+from ...db.session import db_session_dependency
+from .abc import NotificationPayload, NotificationTransport
+
+logger = LOGGER.bind(module=__name__)
+
+
+SLACK_HEADER_SECTION = {
+    StatusEnum.blocked: {
+        "emoji": "ice_cube",
+        "text": "One or more WMS Jobs are BLOCKED",
+    },
+    StatusEnum.failed: {
+        "emoji": "dumpster-fire",
+        "text": "One or more Campaign Nodes have FAILED",
+    },
+    StatusEnum.reviewable: {
+        "emoji": "interrobang",
+        "text": "One or more Campaign Nodes may require REVIEW",
+    },
+    StatusEnum.accepted: {
+        "emoji": "100",
+        "text": "A Campaign or Node is SUCCESSFUL",
+    },
+    StatusEnum.running: {
+        "emoji": "tada",
+        "text": "A Campaign has started RUNNING",
+    },
+    StatusEnum.rejected: {
+        "emoji": "thumbsdown",
+        "text": "A Campaign has been REJECTED",
+    },
+    StatusEnum.overdue: {
+        "emjoji": "calendar",
+        "text": "A Campaign is taking TOO LONG",
+    },
+}
+
+
+class SlackNotification(NotificationTransport):
+    __kind__ = NotificationLabelEnum.slack
+    headers: httpx.Headers = httpx.Headers({"Content-type": "application/json"})
+    slack_webhook_url: str | None = None
+
+    def notify(self, message: bytes | dict) -> None:
+        raise NotImplementedError("Only asynchronous notifications are supported")
+
+    async def anotify(self, message: bytes | dict) -> None:
+        """Sends a Slack notification message asynchronously."""
+
+        if self.slack_webhook_url is None:
+            logger.warning("Cannot produce Slack notification without a webhook url set.")
+            return None
+
+        data = dict(text=message) if isinstance(message, bytes) else message
+
+        async with self.http_async_client() as asession:
+            try:
+                response = await asession.post(
+                    url=self.slack_webhook_url,
+                    json=data,
+                    headers=self.headers,
+                )
+                response.raise_for_status()
+            except httpx.ConnectError as e:
+                logger.error(
+                    "Unable to connect to Slack endpoint",
+                    message=e,
+                )
+            except httpx.HTTPStatusError as e:
+                logger.error(
+                    "Unable to send Slack Notification",
+                    http_status=e.response.status_code,
+                    message=e.response.reason_phrase,
+                )
+
+        return None
+
+    def build_message(self, status: StatusEnum, detail_text: str) -> dict | None:
+        """Construct a Slack Block Kit message"""
+
+        try:
+            use_header = SLACK_HEADER_SECTION[status]
+        except KeyError:
+            return None
+
+        message = {
+            "text": use_header["text"],
+            "blocks": [
+                # rich text header
+                {
+                    "type": "rich_text",
+                    "elements": [
+                        {
+                            "type": "rich_text_section",
+                            "elements": [
+                                {"type": "emoji", "name": use_header["emoji"]},
+                                {"type": "text", "text": use_header["text"]},
+                            ],
+                        }
+                    ],
+                },
+                {"type": "divider"},
+                # detail section
+                {"type": "section", "text": {"type": "mrkdwn", "text": detail_text}},
+                {"type": "divider"},
+                # TODO footer
+            ],
+        }
+        return message
+
+    async def deliver(self, payload: NotificationPayload) -> None:
+        """Deliver a Slack notification based on the payload delivered by the
+        notification handler.
+        """
+        if TYPE_CHECKING:
+            assert db_session_dependency.sessionmaker is not None
+            assert config.notifications.fernet is not None
+
+        # get the log AND the notification label from the database
+        async with db_session_dependency.sessionmaker() as session:
+            try:
+                activity_log = await session.get_one(ActivityLog, payload.id)
+                notification_label = await session.get_one(NotificationLabel, payload.label)
+            except NoResultFound:
+                logger.error(
+                    "Notification subsystem did not find an activity log or "
+                    "notification label when one was specified",
+                    payload=payload,
+                )
+                return None
+
+        # TODO filter the log entry according to rules
+        # (notification_label.configuration?)
+        if activity_log.to_status not in SLACK_HEADER_SECTION:
+            return None
+
+        # TODO build a message from the log entry
+        detail = json.dumps(activity_log.detail) or "Node transitioned"
+        message = self.build_message(activity_log.to_status, detail)
+        if message is None:
+            return None
+
+        # discover the slack webhook url from the label.secret
+        self.slack_webhook_url = config.notifications.fernet.decrypt(notification_label.secret).decode()
+
+        # dispatch the notification with anotify
+        await self.anotify(message)
---
+++ tests/v2/test_notifications.py
@@ -0,0 +1,126 @@
+import asyncio
+from collections.abc import AsyncGenerator
+from textwrap import dedent
+from urllib.parse import urlparse
+from uuid import UUID, uuid5
+
+import pytest
+import pytest_asyncio
+from pytest_mock import MockerFixture
+from sqlalchemy import text
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from lsst.cmservice.config import config
+from lsst.cmservice.db.session import DatabaseManager
+from lsst.cmservice.models.db import Base
+from lsst.cmservice.models.db.campaigns import ActivityLog, Node
+from lsst.cmservice.models.db.notifications import NotificationLabel
+from lsst.cmservice.models.enums import NotificationLabelEnum, StatusEnum
+from lsst.cmservice.notifications.task import Notifier
+
+pytestmark = pytest.mark.asyncio(loop_scope="module")
+"""All tests in this module will run in the same event loop."""
+
+NOTIFICATION_FUNCTION = dedent("""\
+    CREATE OR REPLACE FUNCTION {schema}.notify_event_listeners()
+    RETURNS TRIGGER AS $$
+    DECLARE
+        label_name TEXT;
+        label_kind TEXT;
+        payload TEXT;
+    BEGIN
+        FOREACH label_name in ARRAY NEW.notification_labels
+        LOOP
+            label_kind := NULL;
+            payload := json_build_object(
+                'id', NEW.id::text,
+                'label', label_name
+            );
+            SELECT kind INTO label_kind FROM {schema}.notification_labels_v2 WHERE name = label_name;
+            PERFORM pg_notify(COALESCE(label_kind, 'default'), payload::text);
+        END LOOP;
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+""")
+
+NOTIFICATION_TRIGGER = dedent("""\
+    CREATE TRIGGER notification_events_trigger
+    AFTER INSERT ON {schema}.activity_log_v2
+    FOR EACH ROW
+    EXECUTE FUNCTION {schema}.notify_event_listeners();
+""")
+
+
+@pytest_asyncio.fixture(scope="module", loop_scope="module")
+async def pubsubdb(testdb: DatabaseManager) -> AsyncGenerator:
+    """Set up notification trigger and function on testdb"""
+    assert testdb.engine is not None
+    async with testdb.engine.begin() as aconn:
+        await aconn.execute(text(NOTIFICATION_FUNCTION.format(schema=Base.metadata.schema)))
+        await aconn.execute(text(NOTIFICATION_TRIGGER.format(schema=Base.metadata.schema)))
+        await aconn.commit()
+    yield testdb
+    async with testdb.engine.begin() as aconn:
+        await aconn.execute(text("DROP TRIGGER IF EXISTS notification_events_trigger ON activity_log_v2;"))
+        await aconn.execute(text("DROP FUNCTION IF EXISTS notify_event_listeners();"))
+        await aconn.commit()
+
+
+@pytest_asyncio.fixture(scope="module", loop_scope="module")
+async def notifications_tg(pubsubdb: DatabaseManager) -> AsyncGenerator:
+    shutdown_signal = asyncio.Event()
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(Notifier(None, shutdown_signal).task(), name="notifier")
+        yield
+        shutdown_signal.set()
+
+
+async def test_slack_notifier_task(
+    mocker: MockerFixture, session: AsyncSession, test_campaign_groups: str, notifications_tg: None
+) -> None:
+    """Test the generation of a Slack notification in response to an activity
+    log entry added to the database.
+    """
+    assert config.notifications.fernet is not None
+    # set sentinel for notification call
+    notify_called = asyncio.Event()
+
+    async def notify_side_effect(message: str | bytes) -> None:
+        """set the sentinel event indicating this mock as been called"""
+        notify_called.set()
+
+    mocker.patch(
+        "lsst.cmservice.notifications.transport.slack.SlackNotification.anotify",
+        side_effect=notify_side_effect,
+    )
+    campaign_id = urlparse(url=test_campaign_groups).path.split("/")[-2:][0]
+    node_id = uuid5(UUID(campaign_id), "lambert.1")
+
+    node = await session.get_one(Node, node_id)
+
+    # create a new notification label
+    label = NotificationLabel(
+        name="slack-test",
+        kind=NotificationLabelEnum.slack,
+        configuration={},
+        secret=config.notifications.fernet.encrypt(b"http://mockslack/asdfasdfasdf"),
+    )
+    session.add(label)
+    await session.commit()
+
+    # create a new activity log
+    activity = ActivityLog(
+        namespace=node.campaign.id,
+        node=node.id,
+        operator="test",
+        from_status=StatusEnum.running,
+        to_status=StatusEnum.accepted,
+        detail={},
+        metadata_={},
+        notification_labels=["slack-test"],
+    )
+    session.add(activity)
+    await session.commit()
+
+    await asyncio.wait_for(notify_called.wait(), timeout=5.0)
