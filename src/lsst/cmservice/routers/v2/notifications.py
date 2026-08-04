67f8afec22e4c6008af0345bcd6c8aa3d5b727d8
Toby Jennings <tobyj@slac.stanford.edu>
2026-08-04 10:21:02 -0500

    fix(api): add get collection

--- src/lsst/cmservice/routers/v2/notifications.py
+++ src/lsst/cmservice/routers/v2/notifications.py
@@ -8,16 +8,19 @@ and 409 errors, so specific handling of these cases is implemented only if some
 custom behavior is needed.
 """
 
+from collections.abc import Sequence
 from typing import Annotated
 
 from fastapi import (
     APIRouter,
     Depends,
     HTTPException,
+    Query,
     Request,
     Response,
     status,
 )
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from lsst.cmservice.models.api.notifications import NotificationLabelManifest
@@ -38,6 +41,30 @@ router = APIRouter(
 )
 
 
+@router.get("/", summary="Read the collection of notification labels")
+async def read_notification_label_collection(
+    request: Request,
+    response: Response,
+    session: Annotated[AsyncSession, Depends(db_session_dependency)],
+    limit: Annotated[int, Query(ge=1, le=100)] = 10,
+    offset: Annotated[int, Query(ge=0)] = 0,
+) -> Sequence[NotificationLabel]:
+    """A paginated API returning a list of all Notification Labels known to the
+    application.
+    """
+    statement = select(NotificationLabel)
+    statement = statement.offset(offset).limit(limit)
+    notification_labels = await session.exec(statement)
+
+    # Response header links
+    response.headers["Next"] = str(request.url.include_query_params(offset=(offset + limit), limit=limit))
+    if offset > 0:
+        response.headers["Previous"] = str(
+            request.url.include_query_params(offset=(offset - limit), limit=limit)
+        )
+    return notification_labels.all()
+
+
 @router.get(
     "/{label_name}",
     summary="Read an existing notification label",
@@ -54,7 +81,6 @@ async def read_notification_label_resource(
     # FIXME should probably use a return model that does not have a secret key
     # at all
     label = await session.get_one(NotificationLabel, label_name)
-    ...
     return label
 
 
