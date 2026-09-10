6b4d5f44bc65447a8516013a4896a893d0b38212
Toby Jennings <tobyj@slac.stanford.edu>
2026-09-10 09:41:58 -0500

    fix(group): treat bps unknown status as running
    - the bps "UNKNOWN" status is handled the same as "RUNNING".
    - Defer to the next check.

--- src/lsst/cmservice/machines/nodes/group.py
+++ src/lsst/cmservice/machines/nodes/group.py
@@ -423,7 +423,7 @@ class GroupMachine(NodeMachine, FilesystemActionMixin, HTCondorLaunchMixin):
             f"bps_status_{bps_status.name}",
         )
 
-        if bps_status is WmsStates.RUNNING:
+        if bps_status in {WmsStates.RUNNING, WmsStates.UNKNOWN}:
             return False
 
         # BPS Report
@@ -466,7 +466,7 @@ class GroupMachine(NodeMachine, FilesystemActionMixin, HTCondorLaunchMixin):
                 self.db_model.metadata_["bps_report"][task_name] = wms_dict
 
         # Negative terminal states short-circuit to the machine's error handler
-        if bps_status in {WmsStates.FAILED, WmsStates.DELETED}:
+        if bps_status in {WmsStates.FAILED, WmsStates.DELETED, WmsStates.MISFIT}:
             msg = f"WMS Job is {bps_status.name}"
             raise RuntimeError(msg)
 
