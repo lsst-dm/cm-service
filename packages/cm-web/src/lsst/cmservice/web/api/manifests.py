508c36300d38a2ee9b112bfdbc7eecf7254d39e9
Toby Jennings <tobyj@slac.stanford.edu>
2026-09-23 10:31:54 -0500

    fix(web): wait for setting default
    - when using the "default" button on a campaign manifest card, wait for the api call
    - Use the loading spinner on the default button

--- packages/cm-web/src/lsst/cmservice/web/api/manifests.py
+++ packages/cm-web/src/lsst/cmservice/web/api/manifests.py
@@ -79,11 +79,6 @@ async def put_manifest_list(manifests: list[dict]) -> bool:
     return True
 
 
-async def get_default_manifest_for_campaign(campaign_id: str, kind: str) -> str | None:
-    """..."""
-    ...
-
-
 async def set_default_manifest_for_campaign(manifest: dict) -> bool:
     """Set the given manifest as the default for that campaign-kind"""
     uri = f"/campaigns/{manifest['namespace']}/manifests/{manifest['kind']}/default"
--- packages/cm-web/src/lsst/cmservice/web/components/button.py
+++ packages/cm-web/src/lsst/cmservice/web/components/button.py
@@ -144,9 +144,25 @@ class DefaultManifestButton(ui.button):
     This button's click callback manages its own state, then emits an event to
     any subscribers (provided by a page at construction), which should handle
     the business logic associated with the button's state change.
+
+    Parameters
+    ----------
+    ``is_default``: ``bool``
+        A boolean flag indicating the initial state of the button's selection
+        status. Defaults to ``False``.
+
+    ``manifest``: ``str``
+        A manifest ID with which this button is associated. Defaults to an
+        empty string.
+
+    ``on_completed``: ``Callable`` | ``Awaitable``
+        A callback function (may be async) to which the button's selection
+        ``Event`` is subscribed. Default subscriber creates a basic
+        notification.
     """
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Create a new button instance."""
         self._manifest: str = kwargs.pop("manifest", "")
         self.completed = Event[str]()
         self.completed.subscribe(kwargs.pop("on_completed", self.default_subscriber))
@@ -158,30 +174,35 @@ class DefaultManifestButton(ui.button):
         }
         super().__init__(*args, icon=self._state_icons[self._state], **kwargs)
         self.on("click", self.set_default)
+        self.add_slot("loading", r"<q-spinner-box />")
 
-    def set_default(self) -> None:
+    async def set_default(self) -> None:
         """Set the state if not already set"""
         if self._state:
             return None
         self._state = True
         self._dirty = True
-        self.update()
+        await self.update_default()
 
-    def update(self) -> None:
+    async def update_default(self) -> None:
         """Update button's internal state and emit a completed event to
         subscribers.
+
+        A completion signal is emitted only if the state is "dirty" and the
+        button is associated with a manifest id. Otherwise the update is purely
+        cosmetic.
         """
+        self.props(add="loading")
+        super().update()
         with self.props.suspend_updates():
-            self.set_icon(self._state_icons[self._state])
             if self._dirty and self._manifest:
-                # TODO might want call instead of emit
-                self.completed.emit(self._manifest)
+                await self.completed.call(self._manifest)
                 self._dirty = False
-            else:
-                ...
+        self.props(remove="loading")
+        self.set_icon(self._state_icons[self._state])
         super().update()
 
     @staticmethod
-    def default_subscriber(data: str) -> None:
+    async def default_subscriber(data: str) -> None:
         """Subscriber handler if one is not provided"""
         ui.notify(data)
